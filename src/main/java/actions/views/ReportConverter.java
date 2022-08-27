4c87af7b667a9f70bd54d704d5d1961c438b6997
kouhei.hashimoto <k.hashimoto011235@icloud.com>
2022-08-27 10:24:03 +0900

    Report周りのクラス作成 : DTO、View、Converter、Validator、Service

---
+++ src/main/java/actions/views/ReportConverter.java
@@ -0,0 +1,74 @@
+package actions.views;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Report;
+
+/*
+ * 日報データのDTOモデル ⇔ Viewモデル 相互変換を行うクラス
+ */
+public class ReportConverter {
+
+    /**
+     * ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
+     */
+    public static Report toModel(ReportView rv) {
+        Report r = new Report(
+                rv.getId(),
+                EmployeeConverter.toModel(rv.getEmployee()),
+                rv.getReportDate(),
+                rv.getTitle(),
+                rv.getContent(),
+                rv.getCreatedAt(),
+                rv.getUpdatedAt());
+
+        return r;
+    }
+
+    /**
+     * DTOモデルのインスタンスからViewモデルのインスタンスを作成する
+     */
+    public static ReportView toView(Report r) {
+
+        if(r == null) {
+            return null;
+        }
+
+        ReportView rv = new ReportView(
+                r.getId(),
+                EmployeeConverter.toView(r.getEmployee()),
+                r.getReportDate(),
+                r.getTitle(),
+                r.getContent(),
+                r.getCreatedAt(),
+                r.getUpdatedAt());
+
+        return rv;
+    }
+
+    /**
+     * DTOモデルのリストからViewモデルのリストを作成する
+     */
+    public static List<ReportView> toViewList(List<Report> list) {
+        List<ReportView> viewList = new ArrayList<ReportView>();
+            for(Report r : list) {
+                viewList.add(toView(r));
+            }
+        return viewList;
+    }
+
+    /**
+     * Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
+     */
+    public static void copyViewToModel(Report r, ReportView rv) {
+        r.setId(rv.getId());
+        r.setEmployee(EmployeeConverter.toModel(rv.getEmployee()));
+        r.setReportDate(rv.getReportDate());
+        r.setTitle(rv.getTitle());
+        r.setContent(rv.getContent());
+        r.setCreatedAt(rv.getCreatedAt());
+        r.setUpdatedAt(rv.getUpdatedAt());
+    }
+
+}
